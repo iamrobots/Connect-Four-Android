1a5869fa6686d7d60194850c96aa8abb5de5d7b0
namratha01 <namratha17.bm@gmail.com>
2018-01-26 12:10:05 -0800

    Player modes

---
+++ ConnectFour/app/src/main/java/com/iamrobots/connectfour/PlayerSelection/GameMenuActivity.java
@@ -0,0 +1,20 @@
+package com.iamrobots.connectfour.PlayerSelection;
+
+/**
+ * Created by namrathamanjunatha on 1/26/18.
+ */
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.iamrobots.connectfour.R;
+
+public class GameMenuActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_menu);
+
+    }
+
+}
