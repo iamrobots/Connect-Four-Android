71e33e171d98230e97e4e275fa7bb0961bb96fda
Abraham <aaco2@pdx.edu>
2018-01-29 09:24:33 -0800

    fixed build error with player selection and new player screens, Room not in use yet

--- ConnectFour/app/src/main/java/com/iamrobots/connectfour/PlayerSelection/GameMenuActivity.java
+++ ConnectFour/app/src/main/java/com/iamrobots/connectfour/PlayerSelection/GameMenuActivity.java
@@ -7,6 +7,9 @@ package com.iamrobots.connectfour.PlayerSelection;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.iamrobots.connectfour.R;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class GameMenuActivity extends AppCompatActivity{
 
@@ -15,6 +18,15 @@ public class GameMenuActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_menu);
 
+        Button btnOffline = (Button)findViewById(R.id.button_player);
+
+        btnOffline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(GameMenuActivity.this, PlayerSelectionActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
 }
--- ConnectFour/app/src/main/java/com/iamrobots/connectfour/PlayerSelection/PlayerSelectionActivity.java
+++ ConnectFour/app/src/main/java/com/iamrobots/connectfour/PlayerSelection/PlayerSelectionActivity.java
@@ -6,11 +6,13 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
+import com.iamrobots.connectfour.MainActivity;
 import com.iamrobots.connectfour.R;
 
 public class PlayerSelectionActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,12 +20,41 @@ public class PlayerSelectionActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });*/
+
+        Button btnPlayer1 = (Button)findViewById(R.id.btnPlayer1);
+        btnPlayer1.setOnClickListener(new View.OnClickListener() {
+           @Override
+            public void onClick(View v) {
+               Intent i = new Intent(PlayerSelectionActivity.this, MainActivity.class);
+               startActivity(i);
+           }
+        });
+
+        Button btnPlayer2 = (Button)findViewById(R.id.btnPlayer2);
+        btnPlayer2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(PlayerSelectionActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        // NewPlayerActivity and activity_new_player are the two files i added after finishing
+        // the player select screen.7
+        Button fab = (Button)findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(PlayerSelectionActivity.this, NewPlayerActivity.class);
+                startActivity(i);
             }
         });
     }
