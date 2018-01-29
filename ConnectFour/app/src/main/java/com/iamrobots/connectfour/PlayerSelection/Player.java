9e6eedf1fc7503c46a1f9a2ece5f312ef4cf47ad
namratha01 <namratha17.bm@gmail.com>
2018-01-28 19:13:24 -0800

    Modified for Room changes

--- ConnectFour/app/src/main/java/com/iamrobots/connectfour/PlayerSelection/Player.java
+++ ConnectFour/app/src/main/java/com/iamrobots/connectfour/PlayerSelection/Player.java
@@ -5,5 +5,47 @@ package com.iamrobots.connectfour.PlayerSelection;
  * lively@iamrobots.com
  */
 
-public class Player {
+import android.arch.persistence.room.Room;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class Player extends AppCompatActivity {
+
+    AppDatabase db;
+
+    EditText pName;
+    EditText pColor;
+
+    Button button;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // -- Add your xml part here
+       // setContentView(R.layout.create_user);
+
+        //pName = findViewById(R.id.player_name);
+       // pColor = findViewById(R.id.player_color);
+       // button = findViewById(R.id.button);
+
+        db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "production")
+                .fallbackToDestructiveMigration()
+                .allowMainThreadQueries()
+                .build();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               // PlayerDetls pdt = new PlayerDetls(pName.getText().toString(), pColor.getText().toString());
+                //db.playerDao().insertAll(pdt);
+                //startActivity(new Intent(Player.this, MainActivity.class));
+            }
+        });
+    }
 }
