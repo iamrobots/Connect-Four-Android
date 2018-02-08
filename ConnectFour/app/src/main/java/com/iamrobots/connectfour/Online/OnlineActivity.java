1abe84753c0ed59bca260fa5788dbf877dca2e10
David Lively <lively@iamrobots.com>
2018-02-08 13:43:02 -0800

    Added online activity and top scores activity

---
+++ ConnectFour/app/src/main/java/com/iamrobots/connectfour/Online/OnlineActivity.java
@@ -0,0 +1,17 @@
+package com.iamrobots.connectfour.Online;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.iamrobots.connectfour.R;
+
+public class OnlineActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_online);
+    }
+}
--- ConnectFour/app/src/main/java/com/iamrobots/connectfour/PlayerSelection/GameMenuActivity.java
+++ ConnectFour/app/src/main/java/com/iamrobots/connectfour/PlayerSelection/GameMenuActivity.java
@@ -6,25 +6,52 @@ package com.iamrobots.connectfour.PlayerSelection;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.iamrobots.connectfour.Online.OnlineActivity;
 import com.iamrobots.connectfour.R;
+import com.iamrobots.connectfour.TopScoresActivity;
+
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 
 public class GameMenuActivity extends AppCompatActivity{
 
+    private Button mOfflineButton;
+    private Button mOnlineButton;
+    private Button mTopScoresButton;
+
     @Override
     protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_menu);
 
-        Button btnOffline = (Button)findViewById(R.id.button_player);
+        mOfflineButton = findViewById(R.id.button_player);
+        mOnlineButton = findViewById(R.id.button_multi);
+        mTopScoresButton = findViewById(R.id.button_top_scores);
 
-        btnOffline.setOnClickListener(new View.OnClickListener() {
+        mOfflineButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(GameMenuActivity.this, PlayerSelectionActivity.class);
                 startActivity(i);
+            }
+        });
+
+        mOnlineButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(GameMenuActivity.this, OnlineActivity.class);
+                startActivity(i);
+            }
+        });
+
+        mTopScoresButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(GameMenuActivity.this, TopScoresActivity.class);
+                startActivity(i);
+
             }
         });
     }
