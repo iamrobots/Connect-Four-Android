dee98f336d76b0b851c81b1db051e4dee7d5895b
David Lively <lively@iamrobots.com>
2018-02-22 16:24:17 -0800

    updated version of database and added data members to AIGameActivity

--- ConnectFour/app/src/main/java/com/iamrobots/connectfour/gamePlay/AIGameActivity.java
+++ ConnectFour/app/src/main/java/com/iamrobots/connectfour/gamePlay/AIGameActivity.java
@@ -2,10 +2,41 @@ package com.iamrobots.connectfour.gamePlay;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.iamrobots.connectfour.R;
+import com.iamrobots.connectfour.database.AppDatabase;
 
 public class AIGameActivity extends AppCompatActivity {
+
+    private static final String FIRST_PLAYER_KEY = "PlayerOne";
+    private static final String SECOND_PLAYER_KEY = "PlayerTwo";
+    private static final String ROW_KEY = "Rows";
+    private static final String COLUMNS_KEY = "Columns";
+    private static final String ROUNDS_KEY = "Rounds";
+
+    // Game Layout Components
+    private TextView mFirstPlayerTextView;
+    private TextView mSecondPlayerTextView;
+    private TokenView mFirstPlayerToken;
+    private TokenView mSecondPlayerToken;
+    private BoardView mBoardView;
+    private ImageButton mRewindButton;
+    private Button mRoundsButton;
+
+    // Game Model/State
+    private GameModel mGameModel;
+
+    private int mRounds;
+    private int mCurrentRound;
+    private Boolean mRewindable;
+    private int mPlayerOneWins;
+    private int mPlayerTwoWins;
+
+    private AppDatabase db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
