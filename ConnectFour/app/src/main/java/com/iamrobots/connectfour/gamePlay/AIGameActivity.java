94b33edc5085d6b88b6fa1edae6168305d3b84a3
namratha01 <namratha17.bm@gmail.com>
2018-02-22 17:55:38 -0800

    Random AI Activity

--- ConnectFour/app/src/main/java/com/iamrobots/connectfour/gamePlay/AIGameActivity.java
+++ ConnectFour/app/src/main/java/com/iamrobots/connectfour/gamePlay/AIGameActivity.java
@@ -1,13 +1,21 @@
 package com.iamrobots.connectfour.gamePlay;
 
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Pair;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.iamrobots.connectfour.R;
 import com.iamrobots.connectfour.database.AppDatabase;
+import com.iamrobots.connectfour.database.Player;
 
 public class AIGameActivity extends AppCompatActivity {
 
@@ -37,12 +45,171 @@ public class AIGameActivity extends AppCompatActivity {
 
     private AppDatabase db;
 
+    private Player mHumanPlayer;
+    private AIModel mComputer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        setup();
+        mBoardView.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                v.performClick();
+
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_UP:
+                        switch (mGameModel.getGameState()) {
+                            case 0: // Game is in play
+                                gameInPlay(event.getX(), event.getY());
+                                break;
+
+                            case 1:  // Game is won
+                                break;
+
+                            case 2:  // Game is draw
+                                Toast.makeText(getApplicationContext(), "The game is a draw", Toast.LENGTH_SHORT).show();
+                                break;
+                        }
+                        break;
+                }
+                return true;
+            }
+        });
+
+        mRoundsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mGameModel.getGameState() > 0 && mCurrentRound < mRounds) {
+                    mCurrentRound++;
+                    // newGame();
+                }
+            }
+        });}
+
+    public void gameInPlay(float x, float y) {
+
+        Pair<Integer,Integer> coordinates;
+        int column = mBoardView.getColumn(x);
+        int row = mBoardView.getRow(y);
+
+        //mRewindable = true;
+
+        if (row < 0 || column < 0)
+            return;
+
+        coordinates = mGameModel.dropToken(column);
+
+        if (coordinates == null) {
+            return;
+        }
+
+        mBoardView.dropToken(coordinates.first, coordinates.second, 0);
+
+        if (mGameModel.getGameState() == 1) {
+            mBoardView.highlightTokens(mGameModel.getWinners(), mGameModel.getCurrentPlayer());
+            // gameWon();
+        }
+
+        if (mGameModel.getCurrentPlayer() == 0) {
+            mFirstPlayerToken.selected();
+            mSecondPlayerToken.unselected();
+        } else {
+            mSecondPlayerToken.selected();
+            mFirstPlayerToken.unselected();
+        }
+
+        //mRewindButton.setEnabled(mRewindable);
+
+        //create ai method
+        column = mComputer.dropToken(column);
+        coordinates = mGameModel.dropToken(column);
+        if (coordinates == null) {
+            return;
+        }
+
+        mBoardView.dropToken(coordinates.first,coordinates.second,1);
+
+        if (mGameModel.getGameState() == 1) {
+            mBoardView.highlightTokens(mGameModel.getWinners(), mGameModel.getCurrentPlayer());
+            // gameWon();
+        }
+
+        if (mGameModel.getCurrentPlayer() == 0) {
+            mFirstPlayerToken.selected();
+            mSecondPlayerToken.unselected();
+        } else {
+            mSecondPlayerToken.selected();
+            mFirstPlayerToken.unselected();
+        }
+    }
 
 
+
+
+    private void setup() {
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String firstPlayerName = preferences.getString(FIRST_PLAYER_KEY, "Player 1");
+        int rows = preferences.getInt(ROW_KEY, 6);
+        int columns = preferences.getInt(COLUMNS_KEY, 7);
+        mRounds = preferences.getInt(ROUNDS_KEY, 1);
+        mCurrentRound = 1;
+
+
+        // Temporary Variables. Will get rows and columns from PlayerActivity selection.
+        int firstPlayerColor = Color.parseColor("#f1c40f");
+        int secondPlayerColor = Color.parseColor("#e74c3c");
+
+
+        mFirstPlayerTextView = findViewById(R.id.player1_id);
+        mFirstPlayerTextView.setText(firstPlayerName);
+        mFirstPlayerToken = findViewById(R.id.player1_token_id);
+        mFirstPlayerToken.setColor(firstPlayerColor);
+        mFirstPlayerToken.selected();
+
+        mSecondPlayerTextView = findViewById(R.id.player2_id);
+        mSecondPlayerTextView.setText("Computer");
+        mSecondPlayerToken = findViewById(R.id.player2_token_id);
+        mSecondPlayerToken.setColor(secondPlayerColor);
+        mSecondPlayerToken.unselected();
+
+        mGameModel = new GameModel(rows,columns);
+
+        mBoardView = findViewById(R.id.boardView);
+        mBoardView.setRowsColumns(rows, columns);
+        mBoardView.setFirstPlayerColor(firstPlayerColor);
+        mBoardView.setSecondPlayerColor(secondPlayerColor);
+        mRoundsButton = findViewById(R.id.roundsButton);
+        mRoundsButton.setText("Round " + mCurrentRound + "/" + mRounds);
+        mRoundsButton.setTextColor(Color.BLACK);
+        mRoundsButton.setEnabled(false);
+
+        mRewindable = false;
+        mRewindButton = findViewById(R.id.rewindButton);
+        mRewindButton.setEnabled(mRewindable);
+//        mRewindButton.setVisibility(View.GONE);
+
+        db = AppDatabase.getInstance(this);
+        mHumanPlayer = db.playerDao().getPlayerByName(firstPlayerName);
+        mPlayerOneWins = 0;
+        mPlayerTwoWins = 0;
+        mComputer = new AIModel(rows,columns);
+    }
+
+    public void newGame() {
+        mGameModel.reset();
+        mComputer.reset();
+        mBoardView.clear();
+        mFirstPlayerToken.selected();
+        mSecondPlayerToken.unselected();
+        mRoundsButton.setText("Round " + mCurrentRound + "/" + mRounds);
+        mRoundsButton.setTextColor(Color.BLACK);
+        mRoundsButton.setEnabled(false);
+        mRewindable = false;
+        mRewindButton.setEnabled(mRewindable);
     }
 }
--- ConnectFour/app/src/main/java/com/iamrobots/connectfour/gamePlay/AIModel.java
+++ ConnectFour/app/src/main/java/com/iamrobots/connectfour/gamePlay/AIModel.java
@@ -1,5 +1,9 @@
 package com.iamrobots.connectfour.gamePlay;
 
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Handler;
+
 /**
  * Created by David Lively on 2/22/18.
  * lively@iamrobots.com
@@ -12,12 +16,11 @@ public class AIModel {
     private int mDifficutly;
 
     private int[][] mBoardArray;
-    private int mCurrentPlayer;
+
 
     public AIModel(int rows, int columns) {
         mRows = rows;
         mColumns = columns;
-        mCurrentPlayer = 0;
 
         mBoardArray = new int[rows][columns];
         for (int i = 0; i < mBoardArray.length; i++) {
@@ -26,6 +29,39 @@ public class AIModel {
             }
         }
     }
+
+    public int dropToken(int column)
+    {
+        for(int i=0;i<mRows;i++) {
+            if ( mBoardArray[i][column] == -1)
+            {
+                mBoardArray[i][column] = 0;
+            }
+        }
+        Random rand = new Random();
+        int value = rand.nextInt(mColumns);
+        while (mBoardArray[mRows-1][value] == -1){
+            value = rand.nextInt(mColumns);
+        }
+        for(int i=0;i<mRows;i++) {
+            if ( mBoardArray[i][value] == -1)
+            {
+                mBoardArray[i][value] = 1;
+            }
+        }
+        //add time to wait
+        return value;
+    }
+
+    public void reset(){
+        for (int i = 0; i < mBoardArray.length; i++) {
+            for (int j = 0; j < mBoardArray.length; j++) {
+                mBoardArray[i][j] = -1;
+            }
+        }
+
+    }
+
 
 
 }
