dee4a506f32ac910aefcff57c66df99dbeda837f
David Lively <lively@iamrobots.com>
2018-02-22 18:00:51 -0800

    formating

--- ConnectFour/app/src/main/java/com/iamrobots/connectfour/gamePlay/AIModel.java
+++ ConnectFour/app/src/main/java/com/iamrobots/connectfour/gamePlay/AIModel.java
@@ -30,22 +30,19 @@ public class AIModel {
         }
     }
 
-    public int dropToken(int column)
-    {
-        for(int i=0;i<mRows;i++) {
-            if ( mBoardArray[i][column] == -1)
-            {
+    public int dropToken(int column) {
+        for (int i = 0; i < mRows; i++) {
+            if (mBoardArray[i][column] == -1) {
                 mBoardArray[i][column] = 0;
             }
         }
         Random rand = new Random();
         int value = rand.nextInt(mColumns);
-        while (mBoardArray[mRows-1][value] == -1){
+        while (mBoardArray[mRows - 1][value] == -1) {
             value = rand.nextInt(mColumns);
         }
-        for(int i=0;i<mRows;i++) {
-            if ( mBoardArray[i][value] == -1)
-            {
+        for (int i = 0; i < mRows; i++) {
+            if (mBoardArray[i][value] == -1) {
                 mBoardArray[i][value] = 1;
             }
         }
@@ -53,7 +50,7 @@ public class AIModel {
         return value;
     }
 
-    public void reset(){
+    public void reset() {
         for (int i = 0; i < mBoardArray.length; i++) {
             for (int j = 0; j < mBoardArray.length; j++) {
                 mBoardArray[i][j] = -1;
@@ -61,7 +58,6 @@ public class AIModel {
         }
 
     }
-
 
 
 }
