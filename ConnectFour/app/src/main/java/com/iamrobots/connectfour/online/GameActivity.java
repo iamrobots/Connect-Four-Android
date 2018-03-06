f44f64699d7057003ccb4fea4f1dee6b0ecad625
Aniketha <kmit.aniketha@gmail.com>
2018-03-06 12:09:21 -0800

    Online mode initial commit

---
+++ ConnectFour/app/src/main/java/com/iamrobots/connectfour/online/GameActivity.java
@@ -0,0 +1,286 @@
+package com.iamrobots.connectfour.online;
+
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Pair;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.iamrobots.connectfour.R;
+import com.iamrobots.connectfour.database.AppDatabase;
+import com.iamrobots.connectfour.database.Player;
+import com.iamrobots.connectfour.gamePlay.BoardView;
+import com.iamrobots.connectfour.gamePlay.TokenView;
+import com.iamrobots.connectfour.online.GameModel;
+import com.iamrobots.connectfour.gamePlay.PlayAgainDialog;
+
+import java.util.List;
+
+/*
+ * TODO: Implement a back button that takes the user back to PlayerActivity Selection
+ * TODO: Figure out Database access.
+ * TODO: Implement onPause and onResume
+ */
+
+public class GameActivity extends AppCompatActivity {
+
+    private static final String TAG = "GameActivity";
+
+    private static final String FIRST_PLAYER_KEY = "PlayerOne";
+    private static final String SECOND_PLAYER_KEY = "PlayerTwo";
+    private static final String ROW_KEY = "Rows";
+    private static final String COLUMNS_KEY = "Columns";
+    private static final String ROUNDS_KEY = "Rounds";
+
+    // Game Layout Components
+    private TextView mFirstPlayerTextView;
+    private TextView mSecondPlayerTextView;
+    private TokenView mFirstPlayerToken;
+    private TokenView mSecondPlayerToken;
+    private BoardView mBoardView;
+    private ImageButton mRewindButton;
+    private Button mRoundsButton;
+
+    // Game Model/State
+    private GameModel mGameModel;
+
+
+    private int mRounds;
+    private int mCurrentRound;
+    private Boolean mRewindable;
+    private int mPlayerOneWins;
+    private int mPlayerTwoWins;
+
+    private AppDatabase db;
+    private Player mPlayerOne;
+    private Player mPlayerTwo;
+    private List<String> mScoreList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        setup();
+
+        mBoardView.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                v.performClick();
+
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_UP:
+                        switch (mGameModel.getGameState()) {
+                            case 0: // Game is in play
+                                gameInPlay(event.getX(), event.getY());
+                                break;
+
+                            case 1:  // Game is won
+                                break;
+
+                            case 2:  // Game is draw
+                                if (mCurrentRound < mRounds) {
+                                    mRoundsButton.setText(R.string.next_round);
+                                    mRoundsButton.setEnabled(true);
+                                }
+                                Toast.makeText(getApplicationContext(), "The game is a draw", Toast.LENGTH_SHORT).show();
+                                break;
+                        }
+                        break;
+                }
+                return true;
+            }
+        });
+
+        mRoundsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mGameModel.getGameState() > 0 && mCurrentRound < mRounds) {
+                    mCurrentRound++;
+                    newGame();
+                }
+            }
+        });
+
+        mRewindButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rewind();
+            }
+        });
+    }
+
+    public void gameInPlay(float x, float y) {
+
+        Pair<Integer,Integer> coordinates;
+        int column = mBoardView.getColumn(x);
+        int row = mBoardView.getRow(y);
+
+        mRewindable = true;
+
+        if (row < 0 || column < 0)
+            return;
+
+        int player = mGameModel.getCurrentPlayer();
+        coordinates = mGameModel.dropToken(column);
+
+        if (coordinates == null) {
+            return;
+        }
+
+        mBoardView.dropToken(coordinates.first, coordinates.second, player);
+
+        if (mGameModel.getGameState() == 1) {
+            mBoardView.highlightTokens(mGameModel.getWinners(), mGameModel.getCurrentPlayer());
+            gameWon();
+        }
+
+        if (mGameModel.getCurrentPlayer() == 0) {
+            mFirstPlayerToken.selected();
+            mSecondPlayerToken.unselected();
+        } else {
+            mSecondPlayerToken.selected();
+            mFirstPlayerToken.unselected();
+        }
+
+        mRewindButton.setEnabled(mRewindable);
+    }
+
+    public void gameWon() {
+
+        String winner;
+
+        mRewindable = false;
+        if (mGameModel.getCurrentPlayer() == 0) {
+            winner = mPlayerOne.getName();
+            mPlayerOne.setWins(mPlayerOne.getWins() + 1);
+            mPlayerOneWins += 1;
+            mPlayerTwo.setLosses(mPlayerTwo.getDraws() + 1);
+            mFirstPlayerToken.setScore(String.valueOf(mPlayerOneWins));
+        }
+        else {
+            winner = mPlayerTwo.getName();
+            mPlayerTwo.setWins(mPlayerTwo.getWins() + 1);
+            mPlayerTwoWins += 1;
+            mPlayerOne.setLosses(mPlayerOne.getDraws() + 1);
+            mSecondPlayerToken.setScore(String.valueOf(mPlayerTwoWins));
+        }
+
+        db.playerDao().updatePlayers(mPlayerOne, mPlayerTwo);
+
+        Toast.makeText(this, winner + " is the winner!", Toast.LENGTH_SHORT).show();
+        // added to notify a player with new high score
+        mScoreList = db.playerDao().topScores();
+        if(mScoreList.contains(winner))
+        {
+            Toast.makeText(this, winner + " has a new high score!", Toast.LENGTH_SHORT).show();
+        }
+
+
+        if (mCurrentRound < mRounds) {
+            mRoundsButton.setText(R.string.next_round);
+            mRoundsButton.setEnabled(true);
+        }
+        // else: show dialog to play again
+        else {
+            PlayAgainDialog dialog = new PlayAgainDialog();
+            dialog.show(getFragmentManager(), "AddPlayerDialog");
+        }
+    }
+
+    public void rewind() {
+        Pair<Integer, Integer> rowColumn = mGameModel.rewind();
+        mRewindable = false;
+        mRewindButton.setEnabled(mRewindable);
+
+        if (mGameModel.getCurrentPlayer() == 0) {
+            mFirstPlayerToken.selected();
+            mSecondPlayerToken.unselected();
+        } else {
+            mSecondPlayerToken.selected();
+            mFirstPlayerToken.unselected();
+        }
+
+        if (rowColumn != null) {
+            mBoardView.removeToken(rowColumn.first, rowColumn.second);
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void newGame() {
+        mGameModel.reset();
+        mBoardView.clear();
+        mFirstPlayerToken.selected();
+        mSecondPlayerToken.unselected();
+        mRoundsButton.setText("Round " + mCurrentRound + "/" + mRounds);
+        mRoundsButton.setTextColor(Color.BLACK);
+        mRoundsButton.setEnabled(false);
+        mRewindable = false;
+        mRewindButton.setEnabled(mRewindable);
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void setup() {
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String firstPlayerName = preferences.getString(FIRST_PLAYER_KEY, "Alice");
+        String secondPlayerName = preferences.getString(SECOND_PLAYER_KEY, "Bob");
+        int rows = preferences.getInt(ROW_KEY, 6);
+        int columns = preferences.getInt(COLUMNS_KEY, 7);
+        mRounds = preferences.getInt(ROUNDS_KEY, 1);
+        mCurrentRound = 1;
+
+
+        // Temporary Variables. Will get rows and columns from PlayerActivity selection.
+        int firstPlayerColor = Color.parseColor("#f1c40f");
+        int secondPlayerColor = Color.parseColor("#e74c3c");
+
+
+        mFirstPlayerTextView = findViewById(R.id.player1_id);
+        mFirstPlayerTextView.setText(firstPlayerName);
+        mFirstPlayerToken = findViewById(R.id.player1_token_id);
+        mFirstPlayerToken.setColor(firstPlayerColor);
+        mFirstPlayerToken.selected();
+
+        mSecondPlayerTextView = findViewById(R.id.player2_id);
+        mSecondPlayerTextView.setText(secondPlayerName);
+        mSecondPlayerToken = findViewById(R.id.player2_token_id);
+        mSecondPlayerToken.setColor(secondPlayerColor);
+        mSecondPlayerToken.unselected();
+        //added a new field depth, but this will not affect the actual Game Model
+        mGameModel = new GameModel(rows,columns,1);
+
+        mBoardView = findViewById(R.id.boardView);
+        mBoardView.setRowsColumns(rows, columns);
+        mBoardView.setFirstPlayerColor(firstPlayerColor);
+        mBoardView.setSecondPlayerColor(secondPlayerColor);
+
+        mRoundsButton = findViewById(R.id.roundsButton);
+        mRoundsButton.setText("Round " + mCurrentRound + "/" + mRounds);
+        mRoundsButton.setTextColor(Color.BLACK);
+        mRoundsButton.setEnabled(false);
+
+        mRewindable = false;
+        mRewindButton = findViewById(R.id.rewindButton);
+        mRewindButton.setEnabled(mRewindable);
+//        mRewindButton.setVisibility(View.GONE);
+
+        db = AppDatabase.getInstance(this);
+        mPlayerOne = db.playerDao().getPlayerByName(firstPlayerName);
+        mPlayerTwo = db.playerDao().getPlayerByName(secondPlayerName);
+        mPlayerOneWins = 0;
+        mPlayerTwoWins = 0;
+    }
+
+
+}
---
+++ ConnectFour/app/src/main/java/com/iamrobots/connectfour/online/GameModel.java
@@ -0,0 +1,400 @@
+package com.iamrobots.connectfour.online;
+
+import android.support.annotation.NonNull;
+import android.util.Pair;
+
+import com.iamrobots.connectfour.gamePlay.Move;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+/**
+ * Created by Aniketha Katakam on 1/22/18.
+ *
+ */
+
+// TODO: Add a stack of the columns that where selected to keep track of what was played.
+
+
+public class GameModel {
+    private static final String TAG = GameModel.class.getName();
+
+    private int[][] mBoardArray;
+    private int mBoardRow;
+    private int mBoardColumn;
+    private int mDepth;
+    private int mCurrentPlayer;
+    private int mGameState;
+
+    private ArrayList<Pair<Integer, Integer>> mWinCoordinates;
+    private Stack<Pair<Integer, Integer>> mMoveStack;
+
+    GameModel(int rows, int columns, int depth) {
+        mCurrentPlayer = 0;
+        mGameState = 0;
+        mBoardArray = new int[rows][columns];
+        mBoardRow = rows;
+        mBoardColumn = columns;
+        mDepth = depth;
+        mWinCoordinates = new ArrayList<>();
+        mMoveStack = new Stack<>();
+        for (int i = 0; i < mBoardArray.length; i++)
+        {
+            for (int j = 0; j < mBoardArray[i].length; j++)
+            {
+                mBoardArray[i][j] = -1;
+            }
+        }
+    }
+
+    public int getCurrentPlayer(){
+        return mCurrentPlayer;
+    }
+
+    public void setCurrentPlayer(){
+        mCurrentPlayer = (mCurrentPlayer == 0) ? 1 : 0;
+    }
+
+    public int AIdropToken(int column) {
+        column = getColumn();
+        return column;
+    }
+
+
+    @NonNull
+    private Move chooseMove(int player, int opponent, int alpha, int beta, int depth) {
+        Move best = new Move(-1, player == 1 ? alpha : beta);
+
+        for (int column = 0; column < mBoardColumn; column++) {
+            if (mBoardArray[mBoardRow - 1][column] == -1) {
+                placeToken(player, column);
+                int score = 0;
+                if (checkBoard()) {
+                    score = player == 1 ? 1 : -1;
+                } else if (depth > 1) {
+                    score = chooseMove(opponent, player, alpha, beta, depth - 1).getScore();
+                }
+                removeToken(column);
+
+                if (player == 1 && score > best.getScore()) {
+                    best = new Move(column, score);
+                    alpha = score;
+                } else if (player == 0 && score < best.getScore()) {
+                    best = new Move(column, score);
+                    beta = score;
+                }
+                if (alpha >= beta) {
+                    return best;
+                }
+            }
+        }
+        return best;
+    }
+
+    private int getColumn() {
+        return chooseMove(0, 1, -10000, 10000, mDepth).getColumn();
+    }
+
+    private void placeToken(int player, int column) {
+        for (int i = 0; i < mBoardRow; i++) {
+            if (mBoardArray[i][column] == -1) {
+                mBoardArray[i][column] = player;
+                break;
+            }
+        }
+    }
+
+    private void removeToken(int column) {
+        for (int i = 1; i < mBoardRow; ++i) {
+            if (mBoardArray[i][column] == -1) {
+                mBoardArray[i-1][column] = -1;
+                break;
+            }
+        }
+        mBoardArray[mBoardRow-1][column] = -1;
+    }
+
+    private boolean checkBoard() {
+        boolean horz = AIcheckHorizontal();
+        boolean vert = AIcheckVertical();
+        boolean diagUp = AIcheckDiagonalUpwards();
+        boolean diagDown = AIcheckDiagonalDownwards();
+
+        if (horz|| vert || diagUp || diagDown) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean AIcheckHorizontal(){
+        for(int i = 0; i < mBoardRow; i++ )
+        {
+            for(int j = 0; j <= mBoardColumn -4 ; j++ )
+            {
+                if((mBoardArray[i][j] == 0
+                        && mBoardArray[i][j+1] == 0
+                        && mBoardArray[i][j+2] == 0
+                        && mBoardArray[i][j+3] == 0) || (mBoardArray[i][j] == 1
+                        && mBoardArray[i][j+1] == 1
+                        && mBoardArray[i][j+2] == 1
+                        && mBoardArray[i][j+3] == 1))
+                {
+                    return true;
+                }
+
+            }
+        }
+        return false;
+
+    }
+
+    private boolean AIcheckVertical(){
+        for(int i = 0; i < mBoardColumn; i++ )
+        {
+            for(int j = 0; j <= mBoardRow -4 ; j++ )
+            {
+                if((mBoardArray[j][i] == 0
+                        && mBoardArray[j+1][i] == 0
+                        && mBoardArray[j+2][i] == 0
+                        && mBoardArray[j+3][i] == 0) || (mBoardArray[j][i] == 1
+                        && mBoardArray[j+1][i] == 1
+                        && mBoardArray[j+2][i] == 1
+                        && mBoardArray[j+3][i] == 1))
+                {
+                    return true;
+                }
+
+            }
+        }
+        return false;
+    }
+
+    private boolean AIcheckDiagonalUpwards(){
+        for(int i = 0; i <= mBoardRow -4 ; i++ )
+        {
+            for(int j = 3; j <= mBoardColumn -1  ; j++ )
+            {
+                if((mBoardArray[i][j] == 0
+                        && mBoardArray[i+1][j-1] == 0
+                        && mBoardArray[i+2][j-2] == 0
+                        && mBoardArray[i+3][j-3] == 0) || (mBoardArray[i][j] == 1
+                        && mBoardArray[i+1][j-1] == 1
+                        && mBoardArray[i+2][j-2] == 1
+                        && mBoardArray[i+3][j-3] == 1))
+                {
+                    return true;
+                }
+
+            }
+        }
+        return false;
+    }
+
+    private boolean AIcheckDiagonalDownwards(){
+        for(int i = 0; i <= mBoardRow -4 ; i++ )
+        {
+            for(int j = 0; j <= mBoardColumn -4 ; j++ )
+            {
+                if((mBoardArray[i][j] == 0
+                        && mBoardArray[i+1][j+1] == 0
+                        && mBoardArray[i+2][j+2] == 0
+                        && mBoardArray[i+3][j+3] == 0) || (mBoardArray[i][j] == 1
+                        && mBoardArray[i+1][j+1] == 1
+                        && mBoardArray[i+2][j+2] == 1
+                        && mBoardArray[i+3][j+3] == 1))
+                {
+                    return true;
+                }
+
+            }
+        }
+        return false;
+    }
+
+
+    public Pair<Integer, Integer> dropToken(int column) {
+        int returnRow;
+        for(int i = 0; i < mBoardRow; i++)
+        {
+            if(mBoardArray[i][column] == -1) {
+                mBoardArray[i][column] = getCurrentPlayer();
+                returnRow=i;
+
+                boolean horz = checkHorizontal();
+                boolean vert = checkVertical();
+                boolean diagUp = checkDiagonalUpwards();
+                boolean diagDown = checkDiagonalDownwards();
+
+                if (horz|| vert || diagUp || diagDown) {
+                    mGameState = 1; //game won
+                }
+                else {
+                    int stalemateCounter=0;
+                    int inplayCounter=0;
+                    int boardSize;
+                    //yet to add draw logic
+                    for (int k = 0; k < mBoardArray.length; k++)
+                    {
+                        for (int j = 0; j < mBoardArray[k].length; j++)
+                        {
+                            if(mBoardArray[k][j]== 1 || mBoardArray[k][j]== 0)
+                                stalemateCounter++;
+                            if(mBoardArray[k][j]== -1)
+                                inplayCounter++;
+                        }
+                    }
+
+                    boardSize = (mBoardArray.length)*(mBoardArray[0].length);
+
+                    if(boardSize > inplayCounter )
+                        mGameState = 0;
+                    if(stalemateCounter+1 == boardSize)
+                        mGameState=2;
+                    setCurrentPlayer();
+                }
+                mMoveStack.push(new Pair<>(returnRow, column));
+                return Pair.create(returnRow,column);
+            }
+        }
+
+        return null;
+    }
+
+    // Return Pair<row, column> to be removed
+    public Pair<Integer, Integer> rewind() {
+        if (mMoveStack.isEmpty())
+            return null;
+
+        Pair<Integer, Integer> rowColumnPair = mMoveStack.pop();
+        setCurrentPlayer();
+        mBoardArray[rowColumnPair.first][rowColumnPair.second] = -1;
+        return rowColumnPair;
+    }
+
+    private boolean checkHorizontal(){
+        for(int i = 0; i < mBoardRow; i++ )
+        {
+            for(int j = 0; j <= mBoardColumn -4 ; j++ )
+            {
+                if((mBoardArray[i][j] == 0
+                        && mBoardArray[i][j+1] == 0
+                        && mBoardArray[i][j+2] == 0
+                        && mBoardArray[i][j+3] == 0) || (mBoardArray[i][j] == 1
+                        && mBoardArray[i][j+1] == 1
+                        && mBoardArray[i][j+2] == 1
+                        && mBoardArray[i][j+3] == 1))
+                {
+                    mWinCoordinates.add( Pair.create(i, j));
+                    mWinCoordinates.add( Pair.create(i, j+1));
+                    mWinCoordinates.add( Pair.create(i, j+2));
+                    mWinCoordinates.add( Pair.create(i, j+3));
+                    return true;
+                }
+
+            }
+        }
+        return false;
+
+    }
+
+    private boolean checkVertical(){
+        for(int i = 0; i < mBoardColumn; i++ )
+        {
+            for(int j = 0; j <= mBoardRow -4 ; j++ )
+            {
+                if((mBoardArray[j][i] == 0
+                        && mBoardArray[j+1][i] == 0
+                        && mBoardArray[j+2][i] == 0
+                        && mBoardArray[j+3][i] == 0) || (mBoardArray[j][i] == 1
+                        && mBoardArray[j+1][i] == 1
+                        && mBoardArray[j+2][i] == 1
+                        && mBoardArray[j+3][i] == 1))
+                {
+                    mWinCoordinates.add( Pair.create(j, i));
+                    mWinCoordinates.add( Pair.create(j+1,i));
+                    mWinCoordinates.add( Pair.create(j+2,i));
+                    mWinCoordinates.add( Pair.create(j+3,i));
+                    return true;
+                }
+
+            }
+        }
+        return false;
+    }
+
+    private boolean checkDiagonalUpwards(){
+        for(int i = 0; i <= mBoardRow -4 ; i++ )
+        {
+            for(int j = 3; j <= mBoardColumn -1  ; j++ )
+            {
+                if((mBoardArray[i][j] == 0
+                        && mBoardArray[i+1][j-1] == 0
+                        && mBoardArray[i+2][j-2] == 0
+                        && mBoardArray[i+3][j-3] == 0) || (mBoardArray[i][j] == 1
+                        && mBoardArray[i+1][j-1] == 1
+                        && mBoardArray[i+2][j-2] == 1
+                        && mBoardArray[i+3][j-3] == 1))
+                {
+                    mWinCoordinates.add( Pair.create(i, j));
+                    mWinCoordinates.add( Pair.create(i+1, j-1));
+                    mWinCoordinates.add( Pair.create(i+2, j-2));
+                    mWinCoordinates.add( Pair.create(i+3, j-3));
+                    return true;
+                }
+
+            }
+        }
+        return false;
+    }
+
+    private boolean checkDiagonalDownwards(){
+        for(int i = 0; i <= mBoardRow -4 ; i++ )
+        {
+            for(int j = 0; j <= mBoardColumn -4 ; j++ )
+            {
+                if((mBoardArray[i][j] == 0
+                        && mBoardArray[i+1][j+1] == 0
+                        && mBoardArray[i+2][j+2] == 0
+                        && mBoardArray[i+3][j+3] == 0) || (mBoardArray[i][j] == 1
+                        && mBoardArray[i+1][j+1] == 1
+                        && mBoardArray[i+2][j+2] == 1
+                        && mBoardArray[i+3][j+3] == 1))
+                {
+                    mWinCoordinates.add( Pair.create(i, j));
+                    mWinCoordinates.add( Pair.create(i+1, j+1));
+                    mWinCoordinates.add( Pair.create(i+2, j+2));
+                    mWinCoordinates.add( Pair.create(i+3, j+3));
+                    return true;
+                }
+
+            }
+        }
+        return false;
+    }
+
+    public int getGameState() {
+
+        return mGameState;
+    }
+
+
+    // Return list of winners Pair<row, column>
+    public ArrayList<Pair<Integer, Integer>> getWinners() {
+
+        return mWinCoordinates;
+    }
+
+    public void reset() {
+        mGameState = 0;
+        mCurrentPlayer = 0;
+        mWinCoordinates = new ArrayList<>();
+
+        for (int i = 0; i < mBoardArray.length; i++)
+        {
+            for (int j = 0; j < mBoardArray[i].length; j++)
+            {
+                mBoardArray[i][j] = -1;
+            }
+        }
+    }
+}
