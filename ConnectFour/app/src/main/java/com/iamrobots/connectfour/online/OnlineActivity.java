0ca4870f787b623d80dcca03e1fdaf0bb598a475
Abraham <aaco2@pdx.edu>
2018-03-08 17:09:21 -0800

    Added spinners to online activity and modified edittext.

--- ConnectFour/app/src/main/java/com/iamrobots/connectfour/online/OnlineActivity.java
+++ ConnectFour/app/src/main/java/com/iamrobots/connectfour/online/OnlineActivity.java
@@ -1,6 +1,8 @@
 package com.iamrobots.connectfour.online;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
@@ -10,19 +12,44 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import com.iamrobots.connectfour.R;
 import com.github.nkzawa.socketio.client.IO;
 import com.github.nkzawa.socketio.client.Socket;
 import com.iamrobots.connectfour.TopScoresActivity;
+import com.iamrobots.connectfour.gamePlay.AIGameActivity;
 import com.iamrobots.connectfour.offline.GameHomeActivity;
 import com.iamrobots.connectfour.offline.GameMenuActivity;
+import com.iamrobots.connectfour.offline.PlayerListActivity;
 import com.iamrobots.connectfour.offline.PlayerSelectActivity;
 
-public class OnlineActivity extends AppCompatActivity {
+public class OnlineActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private static final String TAG = "OnlineActivity";
+
+    private static final String FIRST_PLAYER_KEY = "PlayerOne";
+    private static final String SECOND_PLAYER_KEY = "Computer";
+    private static final String ROW_KEY = "Rows";
+    private static final String COLUMNS_KEY = "Columns";
+    private static final String ROUNDS_KEY = "Rounds";
+    private static final String FROM_BUTTON_KEY = "From";
 
     private Button mOnlineButton;
+
+    private Spinner mBoardSizeSpinner;
+    private Spinner mRoundsSpinner;
+
+    private String mFirstPlayerName;
+    private String mSecondPlayerName = "Computer";
+    private int mRows;
+    private int mColumns;
+    private int mRounds;
+
+    private String[] mBoardSizeArray = {"7 x 6", "8 x 7", "10 x 8"};
+    private String[] mRoundsArray = {"1", "2", "3", "4", "5"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +57,40 @@ public class OnlineActivity extends AppCompatActivity {
         setContentView(R.layout.activity_online);
         Toolbar toolbar = findViewById(R.id.toolbar_online);
         setSupportActionBar(toolbar);
+
+        mOnlineButton = findViewById(R.id.button_play);
+
+        mOnlineButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // what we usually pass to the game model in offline mode
+                /*
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString(FIRST_PLAYER_KEY, mFirstPlayerName);
+                editor.putString(SECOND_PLAYER_KEY, mSecondPlayerName);
+                editor.putInt(ROW_KEY, mRows);
+                editor.putInt(COLUMNS_KEY, mColumns);
+                editor.putInt(ROUNDS_KEY, mRounds);
+                editor.apply();
+                */
+
+                Intent i = new Intent(OnlineActivity.this, OnlinedemoActivity.class);
+                startActivity(i);
+            }
+        });
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mFirstPlayerName = preferences.getString(FIRST_PLAYER_KEY, "Alice");
+        mSecondPlayerName = preferences.getString(SECOND_PLAYER_KEY, "Bob");
+        mRows = preferences.getInt(ROW_KEY, 6);
+        mColumns = preferences.getInt(COLUMNS_KEY, 7);
+        mRounds = preferences.getInt(ROUNDS_KEY, 1);
+
+        mBoardSizeSpinner = findViewById(R.id.online_board_spinner);
+        mRoundsSpinner = findViewById(R.id.online_rounds_spinner);
+
+        spinnerSetup();
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.homeBottomBar);
         Menu menu = bottomNavigationView.getMenu();
@@ -54,18 +115,52 @@ public class OnlineActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
 
 
-        mOnlineButton = findViewById(R.id.button_play);
+    /*
+     * Spinner Methods
+     */
 
-        mOnlineButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(OnlineActivity.this, OnlinedemoActivity.class);
-                startActivity(i);
+    public void spinnerSetup() {
+        mBoardSizeSpinner.setOnItemSelectedListener(this);
+        ArrayAdapter<String> boardSizeAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, mBoardSizeArray);
+        boardSizeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mBoardSizeSpinner.setAdapter(boardSizeAdapter);
+
+        mRoundsSpinner.setOnItemSelectedListener(this);
+        ArrayAdapter<String> roundsAdaptor = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, mRoundsArray);
+        roundsAdaptor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mRoundsSpinner.setAdapter(roundsAdaptor);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        if (parent.getId() == R.id.online_board_spinner) {
+            switch (position) {
+                case 0:
+                    mRows = 6;
+                    mColumns = 7;
+                    break;
+                case 1:
+                    mRows = 7;
+                    mColumns = 8;
+                    break;
+                case 2:
+                    mRows = 8;
+                    mColumns = 10;
+                    break;
+                default:
+                    mRows = 6;
+                    mColumns = 7;
             }
-        });
+        } else if (parent.getId() == R.id.online_rounds_spinner) {
+            mRounds = position + 1;
+        }
+    }
 
-
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
     }
 }
